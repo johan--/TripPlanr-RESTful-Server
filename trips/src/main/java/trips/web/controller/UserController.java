c362180960e1932a752015ee96d22bb63d7c29c7
kwoktam <kwoktam@hotmail.com>
2014-06-07 13:04:32 -0700

    remove no used code

--- trips/src/main/java/trips/web/controller/UserController.java
+++ trips/src/main/java/trips/web/controller/UserController.java
@@ -1,20 +1,10 @@
 package trips.web.controller;
 
-import java.io.IOException;
-import java.net.URLDecoder;
-import java.util.UUID;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import trips.model.User;
 import trips.model.dao.UserDao;
 
 @Controller
